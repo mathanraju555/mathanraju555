843588baf56921e19e55ede96c47a65bda1fcc70
mathanraju555 <mathanraju555@gmail.com>
2017-06-05 14:33:56 +0530

    Add files via upload

---
+++ num.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int  main()
+{ int num;
+cin>>num;
+if(num>0)
+{cout<<"is a positive number";}
+else if(num<0)
+{cout<<"negative number";}
+else
+{cout<<"zero";}
+}
