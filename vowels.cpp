0cb906aa2b0db3cd0eeb631754630b4658e26008
mathanraju555 <mathanraju555@gmail.com>
2017-06-05 15:07:08 +0530

    Add files via upload

---
+++ vowels.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    char ch,a,e,i,o,u;
+    cin>>ch;
+if((ch=='a')||(ch=='e')||(ch=='i')||(ch=='o')||(ch=='u'))
+cout<<"vowels";
+else
+cout<<"consonants";
+}
