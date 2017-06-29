6106d9e7e74519af1c94c4a09a34db7369253655
mathanraju555 <mathanraju555@gmail.com>
2017-06-29 10:20:57 +0530

    Add files via upload

---
+++ Strtointeger.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Strtointeger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("enter the string");
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		//Integer a = Integer.valueOf(str);
+	int a=Integer.parseInt(str);
+		System.out.println(a);
+	}
+
+}
